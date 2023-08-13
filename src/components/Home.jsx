4b6a1b070e7cf4596871630a1313743693713e11
web-developer-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-08-13 10:58:03 +0600

    home design done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Lottie from "lottie-react";
+import heroAnime from "../assets/animation/hero-anime.json";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="mt-32 lg:mt-0 container hero">
+      <div>
+        <h1 className="hero-title">
+          Elevate Your Style with Timeless Elegance
+        </h1>
+        <p className="hero-subtitle">
+          Where Modern Trends and Timeless Elegance Converge. Explore a
+          Collection that Redefines Your Wardrobe with Unmatched Style and
+          Sophistication. Elevate Your Fashion Game Today!
+        </p>
+        <div className="hero-buttons">
+          <button className="common-button">
+            <Link to="/shop">Shop Now</Link>
+          </button>
+          <button className="common-button">
+            <Link to="/about">About Us</Link>
+          </button>
+        </div>
+      </div>
+      <div>
+        <Lottie animationData={heroAnime} loop={true} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
