ded24d13e702e07ab2131cb22a28e472459f259c
web-developer-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-08-16 10:53:58 +0600

    View more button added and functionality works perfectly

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "./ProductCard";
 
 const Shop = () => {
-  const products = useLoaderData();
+  const productsData = useLoaderData();
+
+  const [showAll, setShowAll] = useState(false);
+
+  const toggleShowAll = () => {
+    setShowAll(true);
+  };
+
+  const displayProducts = showAll ? productsData : productsData.slice(0, 6);
+
   return (
     <div className="container mt-32 lg:mt-20">
       <div className="product-container">
-        {products.map((product) => (
+        {displayProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+
+      {!showAll && (
+        <div className="view-more-button-container">
+          <button onClick={toggleShowAll} className="common-button">
+            View More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
